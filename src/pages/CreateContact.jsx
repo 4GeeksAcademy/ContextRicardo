fb55b135e4fd2dd20ad0f52fc78c4cde901207f5
Ricky <rikigibson199@gmail.com>
2025-05-24 10:34:41 +0000

    EN PROCESO

--- src/pages/CreateContact.jsx
+++ src/pages/CreateContact.jsx
@@ -1,21 +1,91 @@
+import { useNavigate } from "react-router-dom"
 import { FormContact } from "../components/FormContact"
-
+import useGlobalReducer from "../hooks/useGlobalReducer"
+import { useState } from "react"
+import { prefetchDNS } from "react-dom"
 export const CreateContact = () => {
 
+    const navigate = useNavigate ()
+    const {store,dispatch} = useGlobalReducer ()
+    const [contact, setcontact] = useState ({
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+    })
+
+    const handleChange = (e) => {
+        const {name,value}= e.target
+        setcontact (prev => ({...prev,[name]:value}))
+     } ///maneja cambios en el formulario para guardar la informacion (meter parametro e evento) [name]    }
+    const handleSubmit = async (e) => {
+        e.preventDefault ()
+        try {
+        const response = await fetch("https://playground.4geeks.com/contact/agendas/Ricks/contacts",{
+            method:'POST',
+            headers:{ 
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(contact)
+        })
+        const newContact = await response.json()
+        dispatch ({type:"add_contact",payload:newContact})
+        navigate("/")
+        } catch (error) {
+            console.error ("errorfatal",error)
+        }
+    }
     const createContacts = (body) => {
         fetch('https://playground.4geeks.com/contact/agendas/Ricks/contacts', {
             method: 'POST',
 
             headers : {
-                'content-Type': 'application/Json'
+                'Content-Type': 'application/json'
             }
         })
     }
 
     return  (
-        <div className="container">
+        <div className="container, mb-3">
             <h3>Add new contact</h3>
-            <FormContact title={'New Contact'} onSubmit={createContacts} />
+            <form onsubmit={handleSubmit} className="form-label">
+                <label htmlFor="name"> NEW CONTACT INFO </label>
+                <input
+                type="text"
+                id="email"
+                name="email"
+                value={contact.email}
+                onChange={handleChange}
+                placeholder="Email"
+                />
+                <input
+                type="text"
+                id="address"
+                name="address"
+                value={contact.address}
+                onChange={handleChange}
+                placeholder="Address"
+                />
+                <input
+                type="text"
+                id="phone"
+                name="phone"
+                value={contact.phone}
+                onChange={handleChange}
+                placeholder="phone"
+                />
+                <input
+                type="text"
+                id="name"
+                name="name"
+                value={contact.name}
+                onChange={handleChange}
+                placeholder="Full name"
+                />
+                <button type="button,submit" className="btn btn-primary" onClick={() => navigate("/")}>
+                </button>
+            </form>
+            
         </div>
     )
 }
